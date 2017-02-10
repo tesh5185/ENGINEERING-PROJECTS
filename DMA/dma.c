ec7efa008eccafabf427d3d02f42bf46eba6e2a2
tesh5185 <tesh5185@colorado.edu>
2017-02-09 18:55:44 -0700

    adding the timing profiler

--- DMA/dma.c
+++ DMA/dma.c
@@ -34,12 +34,13 @@
 //static int i = 0;
 void dma_init();
 void dma_use();
-char a[10]={1,2,3,4,5,6,7,8,5,5};
-char b[10]={5,1,2,3,4,6,3,6,4,4};
+char a[5000];
+char b[5000];
 char c[1]={0};
-void dma_memmove(char *src,char *dest,char length);
-void dma_memzero(char *dest,char length);
-void dma_memmove(char *src,char *dest,char length)
+int time=0,count=0;;
+void dma_memmove(char *src,char *dest,uint32_t length);
+void dma_memzero(char *dest,uint32_t length);
+void dma_memmove(char *src,char *dest,uint32_t length)
 {
 	dma_init();
 	DMA_DSR_BCR0|=length;            // total number of transfers
@@ -49,7 +50,7 @@ void dma_memmove(char *src,char *dest,char length)
 		DMA_DCR0|=DMA_DCR_START_MASK; // start transfer
 
 }
-void dma_memzero(char *dest,char length)
+void dma_memzero(char *dest,uint32_t length)
 {
 dma_init();
 DMA_DSR_BCR0|=length;
@@ -64,15 +65,33 @@ void dma_init()
 	SIM->SCGC7|=SIM_SCGC7_DMA_MASK;  // Enable clock for dma
 	DMAMUX0_CHCFG0=0x39;       // give value for source while disabled
 	DMAMUX0_CHCFG0|=DMAMUX_CHCFG_ENBL_MASK;  // enable chanel 0
-
+	DMA_DCR0|=DMA_DCR_SINC_MASK;
     DMA_DCR0|=DMA_DCR_DINC_MASK;  // increment source and destination every transfer
 
-    DMA_DCR0|=DMA_DCR_SSIZE(1)|DMA_DCR_DSIZE(1);  // size of src and dest bus cycle
+    DMA_DCR0|=DMA_DCR_SSIZE(0)|DMA_DCR_DSIZE(0);  // size of src and dest bus cycle
 	/*DMA_DSR_BCR0|=10;            // total number of transfers
 	DMA_SAR0=(uint32_t)&a;  // source address
 		DMA_DAR0=(uint32_t)&b;  //dest address
 */
 }
+void TIMER0_init(){
+		__disable_irq();
+
+		SIM->SOPT2|=0x01000000;		//use mcgfllclk
+		SIM->SCGC6|=0x01000000;		//enable clock to tpm0
+		TPM0->SC=0;					//disable timer
+		TPM0->SC=0x06; 				//prescaler to 64
+		TPM0->MOD=0xFFFF;
+		TPM0->SC|=0x80;				//clear TOF
+		TPM0->SC|=0x40;				//enable timeout interrupt
+		NVIC->ISER[0]|=0x00020000;
+		__enable_irq();
+}
+void TPM0_IRQHandler(void){
+	TPM0->SC|=0x80;				//clear TOF
+	count+=1;
+}
+
 
 int main(void)
 {
@@ -86,10 +105,16 @@ int main(void)
     /* Never leave main */
 //dma_init();
 //dma_memmove(a,b,10);
-	dma_memzero(b,10);
+	TIMER0_init();
+	TPM0->SC|=0x08;				//enable timer
+	dma_memmove(a,b,5000);
+	//dma_memzero(b,5000);
+	TPM0->SC=0;					//disable timer
+	   time=3*((count*65536)+TPM0->CNT);	//time(in microseconds) 3us is the time required by TMP0 counter to increment by 1
 	return 0;
 
 }
 ////////////////////////////////////////////////////////////////////////////////
 // EOF
 ////////////////////////////////////////////////////////////////////////////////
+
