377f9c82fc055b5d52b25b4d9737d94e4dddd7c1
Tejas Shanbhag <tesh5185@colorado.edu>
2017-09-20 17:11:42 -0600

    get working not put and ls

---
+++ udp_client.c
@@ -0,0 +1,169 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <errno.h>
+#include <string.h>
+#define chunk 512
+#define MAXBUFSIZE 25000
+#define MAXFILESIZE 50000
+/* You will have to modify the program below */
+
+int main (int argc, char * argv[])
+{
+
+	int nbytes=1,rem,readbyte=0,sent;                             // number of bytes send by sendto()
+	int sock;                               //this will be our socket
+	FILE *fget=NULL,*fput=NULL;
+	char buffer[MAXFILESIZE];
+	char recbuf[chunk];
+	char direct[MAXBUFSIZE];
+	char put[chunk];
+	/*char *pbuffer=malloc(MAXBUFSIZE*sizeof(char));
+	if(pbuffer==NULL)
+	{
+		printf("malloc failed");
+			
+	}*/	
+
+	//char buffer_ptr=buffer[0];
+	int num=0;
+	struct sockaddr_in remote,dummy;              //"Internet socket address structure"
+	socklen_t remote_length = sizeof(remote);
+	//char get[MAXFILESIZE];
+	//char *get_ptr=&get[0];
+	int recd;
+	if (argc < 3)
+	{
+		printf("USAGE:  <server_ip> <server_port>\n");
+		exit(1);
+	}
+
+	/******************
+	  Here we populate a sockaddr_in struct with
+	  information regarding where we'd like to send our packet 
+	  i.e the Server.
+	 ******************/
+	bzero(&remote,sizeof(remote));               //zero the struct
+	remote.sin_family = AF_INET;                 //address family
+	remote.sin_port = htons(atoi(argv[2]));      //sets port to network byte order
+	remote.sin_addr.s_addr = inet_addr(argv[1]); //sets remote IP address
+
+	//Causes the system to create a generic socket of type UDP (datagram)
+	//if ((sock = **** CALL SOCKET() HERE TO CREATE A UDP SOCKET ****) < 0)
+	if ((sock = socket(AF_INET,SOCK_DGRAM,0)) < 0)	
+	{
+		printf("unable to create socket");
+	}
+	printf("Socket successfully created\n");
+	
+	/******************
+	  sendto() sends immediately.  
+	  it will report an error if the message fails to leave the computer
+	  however, with UDP, there is no error if the message is lost in the network once it leaves the computer.
+	 ******************/
+	char command[MAXBUFSIZE];
+	gets(command);
+	//char command[]="ls";
+	
+	//nbytes = **** CALL SENDTO() HERE ****;
+	nbytes=sendto(sock,command,strlen(command),0,(struct sockaddr *)&remote,sizeof(remote));
+	struct sockaddr_in from_addr;
+	int addr_length = sizeof(struct sockaddr);
+	char *cmp,*token1,*token2;
+	cmp=strstr(command,":");
+	//bzero(token1,sizeof(token1));
+	//token1=command;
+	//bzero(token1,sizeof(token1));
+	printf("command is %s\n",command);
+	if (cmp)
+	{
+		token1=strtok(command,":");
+		token2=strtok(NULL,":");
+		printf("cmd is %s & file is %s\n",token1,token2);
+
+	}
+	
+	
+	
+	num=strcmp(token1,"get");
+	if(num==0)
+	{
+		printf("num=%d",num);
+		bzero(recbuf,sizeof(recbuf));
+		nbytes=chunk;
+		fget=fopen(token2,"wb");
+		while(nbytes>=chunk)
+		{	
+			nbytes=recvfrom(sock,recbuf,chunk,0,(struct sockaddr *)&remote,&remote_length);
+		
+			//strncpy(buffer,recbuf,nbytes);
+			printf("bytes recieved=%d\n",nbytes);
+			recd+=nbytes;
+			fwrite(recbuf,1,nbytes, fget);
+			
+		}	
+		printf("received size is %d\n",recd);
+	fclose(fget);
+	}
+	else
+	{
+		num=strcmp(token1,"put");
+		if(num==0)
+		{
+			fput=fopen(token2,"rb");	
+			fseek(fput, 0 , SEEK_END);	
+			int fsize = ftell(fput);
+			printf("File size is %d\n", fsize);
+			fseek(fput, 0, SEEK_SET);
+			rem=fsize;
+			while(rem>0)
+			{	
+				if(rem>chunk)
+					readbyte=chunk;
+				else
+					readbyte=rem;
+	
+				size_t bytes_read=fread(put, sizeof(char),readbyte, fput);			
+				if (bytes_read!=readbyte*sizeof(char))
+					printf("Incomplete read. Read : %d, Expected : %ld\n", (int)bytes_read,(readbyte*sizeof(char)));
+
+				printf("Sending %d bytes to client\n", chunk);
+				nbytes=sendto(sock,put,readbyte,0,(struct sockaddr *)&remote,&remote_length);
+		
+				printf("sent size is %d\t",readbyte);
+				rem-=chunk;
+				printf("rem=%d\n ",rem);
+				sent+=nbytes;
+			}
+			fclose(fput);
+
+		}
+	}
+
+
+
+
+	bzero(direct,sizeof(direct));
+	num=strcmp(command,"ls");nbytes=0;
+	//if(strcmp(command,"ls")==0);
+	if(num==0)	
+	{
+		printf("command is %s\n",command);
+		nbytes=recvfrom(sock,direct,MAXBUFSIZE,0,(struct sockaddr *)&remote,sizeof(remote));
+		printf("The list is %s\n",direct);		
+	}
+	
+	
+	close(sock);
+
+}
+
---
+++ udp_server.c
@@ -0,0 +1,189 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#define chunk 512
+/* You will have to modify the program below */
+
+#define MAXBUFSIZE 500
+#define MAXFILESIZE 10000
+int main (int argc, char * argv[] )
+{
+
+	//printf("start\n");
+	FILE *fget=NULL,*fput=NULL;
+	int sock;                           //This will be our socket
+	struct sockaddr_in serv, remote;     //"Internet socket address structure"
+	socklen_t remote_length = sizeof(remote);       //length of the sockaddr_in structure
+	int nbytes;                        //number of bytes we receive in our message
+	unsigned char buffer[MAXBUFSIZE];             //a buffer to store our received message
+	DIR *d;
+	char delimiter[]=":";
+	struct dirent *dir;
+	char list[MAXBUFSIZE],recbuf[chunk]; 
+	char *list_ptr=&list[0];
+	char num=0;
+	int readbyte=0;
+	d = opendir(".");
+	unsigned char filedata[chunk];
+	unsigned char *buffer_ptr=malloc(MAXFILESIZE*sizeof(char));
+	int sent;
+	int err,recd=0;
+	int rem;
+	if (argc != 2)
+	{
+		printf ("USAGE:  <port>\n");
+		exit(1);
+	}
+	
+	/******************
+	  This code populates the sockaddr_in struct with
+	  the information about our socket
+	 ******************/
+	bzero(&serv,sizeof(serv));                    //zero the struct
+	serv.sin_family = AF_INET;                   //address family
+	serv.sin_port = htons(atoi(argv[1]));        //htons() sets the port # to network byte order
+	serv.sin_addr.s_addr = INADDR_ANY;           //supplies the IP address of the local machine
+
+	//printf("Creating socket\n");
+	//Causes the system to create a generic socket of type UDP (datagram)
+	//if ((sock = **** CALL SOCKET() HERE TO CREATE UDP SOCKET ****) < 0)
+	
+	if ((sock = socket(AF_INET,SOCK_DGRAM,0)) < 0)
+	{
+	    printf("unable to create socket, sock : %d\n", sock);
+	}
+	printf("Socket successfully created\n");
+
+
+	/******************
+	  Once we've created a socket, we must bind that socket to the 
+	  local address and port we've supplied in the sockaddr_in struct
+	 ******************/
+	if (bind(sock, (struct sockaddr *)&serv, sizeof(serv)) < 0)
+	{
+		perror("unable to bind socket\n");
+		
+	}
+	printf("Socket successfully binded\n");
+	
+
+	//waits for an incoming message
+	bzero(buffer,sizeof(buffer));
+	nbytes=recvfrom(sock,buffer,MAXBUFSIZE,0,(struct sockaddr *)&remote,&remote_length);
+	printf("The client says %s\n", buffer);
+	char *cmp, *token1, *token2;
+	cmp=strstr(buffer,delimiter);
+	if(cmp)
+	{
+		token1=strtok(buffer,delimiter);
+		token2=strtok(NULL,delimiter);
+		strcpy(buffer_ptr,token2);
+		printf("The strings are %s and %s\n",token1,token2);
+		printf("not here");
+		num=strcmp(token1,"get");
+		if(num==0)		//if(strcmp(token1,"get")==0);
+		{ 	
+				
+			printf("Sending file %s\n",buffer_ptr);
+			bzero(filedata,sizeof(filedata));	
+			printf("filename is %s\n", buffer_ptr);
+			fget=fopen(buffer_ptr,"rb");
+			fseek(fget, 0 , SEEK_END);	
+			int fsize = ftell(fget);
+			printf("End : %p\n", fget);
+			printf("File size is %d\n", fsize);
+			fseek(fget, 0, SEEK_SET);
+			rem=fsize;
+			while(rem>0)
+			{	
+				if(rem>chunk)
+					readbyte=chunk;
+				else
+					readbyte=rem;
+	
+				size_t bytes_read=fread(filedata, sizeof(char),readbyte, fget);			
+				if (bytes_read!=readbyte*sizeof(char))
+					printf("Incomplete read. Read : %d, Expected : %ld\n", (int)bytes_read,(readbyte*sizeof(char)));
+
+				printf("Sending %d bytes to client\n", chunk);
+				nbytes=sendto(sock,filedata,readbyte,0,(struct sockaddr *)&remote,sizeof(remote));
+		
+				printf("sent size is %d\t",readbyte);
+				rem-=chunk;
+				printf("rem=%d\n ",rem);
+				sent+=nbytes;
+			}
+			fclose(fget);
+		}
+		
+		 printf("please send");
+			num=strcmp(token1,"put");
+			if(num==0)
+				{
+					nbytes=chunk;
+					fput=fopen(token2,"wb");
+					while(nbytes>=chunk)
+					{	
+						nbytes=recvfrom(sock,recbuf,chunk,0,(struct sockaddr *)&remote,&remote_length);
+		
+						//strncpy(buffer,recbuf,nbytes);
+						printf("bytes recieved=%d\n",nbytes);
+						recd+=nbytes;
+						fwrite(recbuf,1,nbytes, fput);
+			
+					}	
+					printf("received size is %d\n",recd);
+					fclose(fput);
+				}
+		
+	}	
+		num=strcmp(buffer,"ls");
+		printf("num=%d",num);
+		//if(strcmp(buffer,"ls")==0);
+		if(num==0)
+		{	
+			printf("buffer is %s\n",buffer);
+			memset(list,0,sizeof(list));
+	
+			printf("listing the files\n");	
+			if (d)
+			{
+				while ((dir = readdir(d)) != NULL)
+				{
+					//printf("Directory entry : %s\n",dir->d_name);
+					//printf("%p\n",list);
+					strncpy(list_ptr,dir->d_name,strlen(dir->d_name));
+					//printf("%p\n",list);
+					list_ptr+= strlen(dir->d_name);
+					strncpy(list_ptr,"\n",strlen("\n"));
+					list_ptr+= strlen("\n");
+				}
+				nbytes=sendto(sock,list,strlen(list),0,(struct sockaddr *)&remote,sizeof(remote));
+			}
+			printf("List is\n %s\n string size=%d\n",list,(int)strlen(list));
+			closedir(d);
+		}
+		if(buffer_ptr!=NULL)	
+		free(buffer_ptr);
+
+	/*printf("Sending %d bytes to client\n", strlen(filedata));
+	nbytes=sendto(sock,filedata,fsize,0,(struct sockaddr *)&remote,sizeof(remote));
+	*///nbytes = **** CALL SENDTO() HERE ****;
+//	printf("Total byes sent=%d\n",sent);
+	close(sock);
+}
+
